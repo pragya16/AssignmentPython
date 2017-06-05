49edac9040860bf8e08107972fc0bdb0548ebce7
pragya16 <pragya.g16@gmail.com>
2017-06-05 22:59:34 +0530

    Add files via upload

---
+++ program5.py
@@ -0,0 +1,18 @@
+# Question 5 :Define a function that can accept two strings as input and concatenate them and then print it in console
+
+
+def func(t1,t2):
+        ret= t1+t2
+        return ret
+
+t1= sys.argv[1]
+t2= sys.argv[2]
+
+
+
+'''
+output:
+[root@demo pragya_g16]# python fifth.py
+pragyagupta
+
+'''
