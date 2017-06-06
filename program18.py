a3de0599384181ee210b3ffade6e44c541285270
pragya16 <pragya.g16@gmail.com>
2017-06-06 12:58:08 +0530

    Create program18.py

---
+++ program18.py
@@ -0,0 +1,16 @@
+# Question 18 : Write a program which accepts a string from console and print it in reverse order
+
+str=raw_input("enter your input")
+print "input is:",str
+str=str[::-1]
+print str
+
+
+'''
+output:
+[root@demo pragya_g16]# python eighteenth.py
+enter your input astha
+input is:  astha
+ahtsa 
+
+'''
