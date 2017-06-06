6a59fa2f9b6d092adffdbaaa85c2ae89533c50e3
pragya16 <pragya.g16@gmail.com>
2017-06-06 14:19:41 +0530

    Create program14.py

---
+++ program14.py
@@ -0,0 +1,11 @@
+# Question 14: Write a program to read an ASCII string and to convert it to a unicode string encoded by utf-8.
+
+s = raw_input("Enter the ASCII Value")
+u = unicode( s ,"utf-8")
+print u
+
+'''
+Output:
+[root@demo pragya_g16]# python forteen.pyEnter the ASCII ValueAA 
+
+ '''
