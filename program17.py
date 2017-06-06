836dd6cbe3b0bb45ee6575f3e54d7eb97e646c09
pragya16 <pragya.g16@gmail.com>
2017-06-06 14:43:01 +0530

    changing

--- program17.py
+++ program17.py
@@ -1,15 +1,16 @@
 # Question 17: With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155], write a program to make a list whose elements are intersection of the above given lists
 
 
-def interchange(A, B):
-    """ return the interchange of two lists """
-    return list(set(A) &  set(B))
+
+def intersect(a, b):
+    """ return the intersection of two lists """
+    return list(set(a) &  set(b))
 
 
 if __name__ == "__main__": 
-    A = [1,3,6,78,35,55]
-    B = [12,24,35,24,88,120,155]
-    print interchange(A, B)
+    a = [1,3,6,78,35,55]
+    b = [12,24,35,24,88,120,155]
+    print intersect(a, b)
 
 '''
 output:
