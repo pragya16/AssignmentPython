1beaff0066087edc4fc55eae37164dcbd8654027
pragya16 <pragya.g16@gmail.com>
2017-06-06 12:54:33 +0530

    Create program11.py

---
+++ program11.py
@@ -0,0 +1,27 @@
+# Question 11:Write a program that accepts a sentence and calculate the number of letters and digits.
+              Suppose the following input is supplied to the program:
+              i/p: Hello Priya 1287
+              o/p: LETTERS 10
+                     DIGITS 4
+
+
+str=raw_input("enter string") d=l=0
+for c in str:        
+	if c.isdigit():                
+		d=d+1        
+	elif c.isalpha():                
+		l=l+1        
+	else:                
+		pass
+print("letters",l)
+print("digits",d)
+
+
+'''
+output:
+[root@demo pragya_g16]# python eleven,py
+enter string pragya
+('letters', 6)
+('digits', 0)
+
+'''
