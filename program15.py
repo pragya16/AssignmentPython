2cfcd4105671d431478394d3aa44460b9964c043
pragya16 <pragya.g16@gmail.com>
2017-06-06 12:56:15 +0530

    Create program15.py

---
+++ program15.py
@@ -0,0 +1,12 @@
+# Question 15: Write a program to shuffle and print the list [3,6,7,8].
+ from random import shuffle
+li = [3,6,7,8]
+shuffle(li)
+print li
+
+'''
+output:
+[root@demo pragya_g16]# python fifteen.py
+[7, 8, 3, 6]
+
+'''
