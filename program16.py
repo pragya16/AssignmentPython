e34e3586c801750047eef7caf600fa51a494e7a2
pragya16 <pragya.g16@gmail.com>
2017-06-06 15:25:46 +0530

    Update program16.py
    ass 16

--- program16.py
+++ program16.py
@@ -1,19 +1,15 @@
 # Question 16 : Write a program to print the list after removing delete even numbers in [5,6,77,45,22,12,24]
 
-def remove_even(l):    
-	for i in l:        
-		if i % 2 == 0:            
-			l.remove(i)   
- 	print l    
-return l
-remove_even([5,6,77,45,22,12,24])
-remove_even([5,77,45,12])
+li = [5,6,77,45,22,12,24] 
+li = [x for x in li if x%2!=0] 
+print li 
+
 
 
 '''
 output:
 [root@demo pragya_g16]# python sixteen.py
 [5, 77, 45, 12]
-[5, 77, 45]
+
 
 '''
